2ebcd382456a374dd676c485c1462e8899a80453
Jeffry Gonzalez <jeff@hypertheory.com>
2021-06-13 14:37:33 -0400

    basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FourOhFourComponent } from './components/four-oh-four/four-oh-four.component';
+import { HomeComponent } from './components/home/home.component';
+import { ParksComponent } from './components/parks/parks.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'parks',
+    component: ParksComponent
+  },
+  {
+    path: '**',
+    component: FourOhFourComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './components/home/home.component';
+import { ParksComponent } from './components/parks/parks.component';
+import { FourOhFourComponent } from './components/four-oh-four/four-oh-four.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    ParksComponent,
+    FourOhFourComponent
   ],
   imports: [
     BrowserModule,
